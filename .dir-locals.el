b52fda978e942527bc48733398532358e48a65fb
Ruslan Kiselev <voltecrus@gmail.com>
2018-04-03 10:10:15 +0300

    Added .dir-locals for emacs development

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cider-lein-parameters . "with-profiles +cider repl :headless")
+	 (cider-cljs-lein-repl . "(do (require 'figwheel-sidecar.repl-api)
+           (figwheel-sidecar.repl-api/start-figwheel!)
+           (figwheel-sidecar.repl-api/cljs-repl))"))))
